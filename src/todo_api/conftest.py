252c2d3a3582752280caffdfd95aefd88b792026
karwa <karwatmateusz07@gmail.com>
2023-04-23 12:02:52 +0200

    new test structure

--- src/todo_api/conftest.py
+++ src/todo_api/conftest.py
@@ -7,7 +7,7 @@ import requests
 @pytest.fixture(scope="class", autouse=False)
 def create_todo():
     url_todo = "https://apichallenges.herokuapp.com/todos"
-    todo_title = f"title {str(uuid4())}"
+    todo_title = f"title"
     payload = dumps(
         {
             "title": todo_title,
@@ -16,17 +16,10 @@ def create_todo():
         }
     )
     headers = {"Content-Type": "application/json", "Accept": "application/json"}
-
     response = requests.post(url=url_todo, data=payload, headers=headers)
-
-    assert (
-        response.status_code == 201
-    ), f"Request error, wrong status code - {response.status_code}"
-
-    todo_list = requests.get(url_todo)
-    modified_list = todo_list.json()["todos"]
-    new_todo = [todo for todo in modified_list if todo["title"] == todo_title][0]
-    todo_id = new_todo["id"]
-    assert len(new_todo) != 0, f"New todo not added into the list"
-
-    yield todo_id
+    if response.status_code == 204:
+        todo_list = requests.get(url_todo)
+        modified_list = todo_list.json()["todos"]
+        new_todo = [todo for todo in modified_list if todo["title"] == todo_title][0]
+        todo_id = new_todo["id"]
+        yield todo_id
--- src/todo_api/test_todos.py
+++ src/todo_api/test_todos.py
@@ -10,17 +10,19 @@ class TestApi:
     _todo_title = f"title {str(uuid4())}"
 
     @pytest.mark.api
-    def test_pay_invoices_in_todo_list(self):
+    def test_todo_list_is_not_empty(self):
         response = requests.get(self._url_todo)
         response_text = response.json()["todos"]
         assert (
             response.status_code == 200
         ), f"Request error, wrong status code - {response.status_code}"
-        todo_title = [todo["title"] for todo in response_text]
-        assert len(todo_title) != 0, f"List is empty"
+        todo_list = [todo for todo in response_text]
+        assert (
+            len(todo_list) != 0
+        ), f"Expected to find at least one todo item in the list ({len(todo_list)} found)"
 
     @pytest.mark.api
-    def test_new_todo_can_be_added(self):
+    def test_todo_can_be_created(self):
         payload = dumps(
             {
                 "title": self._todo_title,
@@ -44,7 +46,7 @@ class TestApi:
 
     @pytest.mark.dependency(depends=["create_todo"])
     @pytest.mark.api
-    def test_specific_todo_can_be_retrieved(self, create_todo):
+    def test_todo_can_be_retrieved_by_id(self, create_todo):
         todo_id = create_todo
         response = requests.get(f"{self._url_todo}/{todo_id}")
         response_text = response.json()
@@ -52,11 +54,13 @@ class TestApi:
             response.status_code == 200
         ), f"Request error, wrong status code - {response.status_code}"
         pprint(f"retrievew todo {response_text}")
-        assert len(response_text) == 1, f"List is empty"
+        assert (
+            len(response_text) == 1
+        ), f"Expected to retrieve one todo item (id={todo_id}), but found {len(response_text)} items"
 
     @pytest.mark.dependency(depends=["create_todo"])
     @pytest.mark.api
-    def test_specific_todo_can_be_updated(self, create_todo):
+    def test_todo_can_be_updated_by_id(self, create_todo):
         todo_id = create_todo
         payload = dumps(
             {
@@ -65,18 +69,24 @@ class TestApi:
         )
         headers = {"Content-Type": "application/json", "Accept": "application/json"}
 
-        url_to_specific_todo = f"{self._url_todo}/{todo_id}"
-        response = requests.post(
-            url=url_to_specific_todo, data=payload, headers=headers
-        )
+        specific_todo_url = f"{self._url_todo}/{todo_id}"
+        response = requests.post(url=specific_todo_url, data=payload, headers=headers)
         assert (
             response.status_code == 200
-        ), f"Request failed, todo not updated - status code {response.status_code}"
+        ), f"Failed to update todo item (id={todo_id}, title='new updated title'), status code {response.status_code}"
 
-        updated_response = requests.get(url_to_specific_todo)
+        updated_response = requests.get(specific_todo_url)
         updated_todo = updated_response.json()
         print(f"updated todo is {updated_todo}")
 
     @pytest.mark.api
-    def test_specific_todo_can_be_deleted(self):
-        pass
+    def test_todo_can_be_deleted_by_id(self, create_todo):
+        todo_id = create_todo
+        specific_todo_url = f"{self._url_todo}/{todo_id}"
+        response = requests.delete(specific_todo_url)
+        assert (
+            response.status_code == 200
+        ), f"Failed to delete todo item (id={todo_id}), status code {response.status_code}"
+
+        specific_todo_item = requests.get(specific_todo_url)
+        assert specific_todo_item.status_code == 404
