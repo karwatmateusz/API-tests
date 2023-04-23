77b473a1dba58d53a62cac3a0c84a2a6cb58aab0
karwa <karwatmateusz07@gmail.com>
2023-04-23 13:26:09 +0200

    add tests for login functionality

--- src/test_users/test_login.py
+++ src/test_users/test_login.py
@@ -3,49 +3,49 @@ import requests
 from json import dumps
 
 
-class TestUserRegistration:
+class TestUserLogin:
     def setup_method(self):
         self.base_api_url = "https://reqres.in/api"
-        self.registration_endpoint = f"{self.base_api_url}/register"
+        self.login_endpoint = f"{self.base_api_url}/login"
         self.user_mail = "tracey.ramos@reqres.in"
         self.user_password = "some_password"
         self.header = {"Content-Type": "application/json"}
 
-    @pytest.mark.registration
-    def test_registration_return_token(self):
+    @pytest.mark.login
+    def test_user_can_login_with_valid_credentials(self):
         payload = dumps({"email": self.user_mail, "password": self.user_password})
         response = requests.post(
-            url=self.registration_endpoint, data=payload, headers=self.header
+            url=self.login_endpoint, headers=self.header, data=payload
         )
         assert response.status_code == 200
         assert "token" in response.json()
 
-    @pytest.mark.registration
-    def test_user_cant_register_without_password(self):
-        payload = dumps({"email": self.user_mail})
+    @pytest.mark.login
+    def test_user_not_logged_with_invalid_credentials(self):
+        payload = dumps({"email": "invalid@email.com", "password": "somepassword"})
         response = requests.post(
-            url=self.registration_endpoint, data=payload, headers=self.header
+            url=self.login_endpoint, headers=self.header, data=payload
         )
         assert response.status_code == 400
         error_message = response.json()["error"]
-        assert "Missing password" in error_message
+        assert "user not found" in error_message
 
-    @pytest.mark.registration
-    def test_user_cant_register_without_email(self):
+    @pytest.mark.login
+    def test_user_not_logged_without_email(self):
         payload = dumps({"password": self.user_password})
         response = requests.post(
-            url=self.registration_endpoint, data=payload, headers=self.header
+            url=self.login_endpoint, headers=self.header, data=payload
         )
         assert response.status_code == 400
         error_message = response.json()["error"]
         assert "Missing email or username" in error_message
 
-    @pytest.mark.registration
-    def test_register_not_defined_user(self):
-        payload = dumps({"email": "some_user", "password": "anypassword"})
+    @pytest.mark.login
+    def test_user_not_logged_without_password(self):
+        payload = dumps({"email": self.user_mail})
         response = requests.post(
-            url=self.registration_endpoint, data=payload, headers=self.header
+            url=self.login_endpoint, headers=self.header, data=payload
         )
         assert response.status_code == 400
         error_message = response.json()["error"]
-        assert "Only defined users succeed registration" in error_message
+        assert "Missing password" in error_message
