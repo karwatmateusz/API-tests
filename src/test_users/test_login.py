a5992ebb6d25deac94abd568f5e512d0d9e85626
karwa <karwatmateusz07@gmail.com>
2023-04-23 15:02:33 +0200

    create fixtures with requests payload

--- src/test_users/test_login.py
+++ src/test_users/test_login.py
@@ -7,45 +7,63 @@ class TestUserLogin:
     def setup_method(self):
         self.base_api_url = "https://reqres.in/api"
         self.login_endpoint = f"{self.base_api_url}/login"
-        self.user_mail = "tracey.ramos@reqres.in"
-        self.user_password = "some_password"
         self.header = {"Content-Type": "application/json"}
 
-    @pytest.mark.login
-    def test_user_can_login_with_valid_credentials(self):
-        payload = dumps({"email": self.user_mail, "password": self.user_password})
-        response = requests.post(
-            url=self.login_endpoint, headers=self.header, data=payload
-        )
-        assert response.status_code == 200
-        assert "token" in response.json()
+    @pytest.fixture
+    def valid_user_credentials(self):
+        user_mail = "tracey.ramos@reqres.in"
+        user_password = "some_password"
+        return user_mail, user_password
+
+    @pytest.fixture
+    def valid_user_payload(self, valid_user_credentials):
+        email, password = valid_user_credentials
+        return {"email": email, "password": password}
+
+    @pytest.fixture
+    def invalid_user_credentials(self):
+        user_mail = "@mail.com"
+        user_password = "testpass"
+        return user_mail, user_password
+
+    @pytest.fixture
+    def invalid_user_payload(self, invalid_user_credentials):
+        email, password = invalid_user_credentials
+        return {"email": email, "password": password}
 
     @pytest.mark.login
-    def test_user_not_logged_with_invalid_credentials(self):
-        payload = dumps({"email": "invalid@email.com", "password": "somepassword"})
-        response = requests.post(
-            url=self.login_endpoint, headers=self.header, data=payload
-        )
-        assert response.status_code == 400
-        error_message = response.json()["error"]
-        assert "user not found" in error_message
+    def test_user_can_login_with_valid_credentials(self, valid_user_payload):
+        with requests.post(
+            url=self.login_endpoint, headers=self.header, json=valid_user_payload
+        ) as response:
+            assert response.status_code == 200
+            assert "token" in response.json()
 
     @pytest.mark.login
-    def test_user_not_logged_without_email(self):
-        payload = dumps({"password": self.user_password})
-        response = requests.post(
-            url=self.login_endpoint, headers=self.header, data=payload
-        )
-        assert response.status_code == 400
-        error_message = response.json()["error"]
-        assert "Missing email or username" in error_message
+    def test_user_not_logged_with_invalid_credentials(self, invalid_user_payload):
+        with requests.post(
+            self.login_endpoint, headers=self.header, json=invalid_user_payload
+        ) as response:
+            assert response.status_code == 400
+            error_message = response.json()["error"]
+            assert "user not found" in error_message
 
     @pytest.mark.login
-    def test_user_not_logged_without_password(self):
-        payload = dumps({"email": self.user_mail})
-        response = requests.post(
-            url=self.login_endpoint, headers=self.header, data=payload
-        )
-        assert response.status_code == 400
-        error_message = response.json()["error"]
-        assert "Missing password" in error_message
+    def test_user_not_logged_without_email(self, valid_user_payload):
+        del valid_user_payload["email"]
+        with requests.post(
+            url=self.login_endpoint, headers=self.header, json=valid_user_payload
+        ) as response:
+            assert response.status_code == 400
+            error_message = response.json()["error"]
+            assert "Missing email or username" in error_message
+
+    @pytest.mark.login
+    def test_user_not_logged_without_password(self, valid_user_payload):
+        del valid_user_payload["password"]
+        with requests.post(
+            url=self.login_endpoint, headers=self.header, json=valid_user_payload
+        ) as response:
+            assert response.status_code == 400
+            error_message = response.json()["error"]
+            assert "Missing password" in error_message
